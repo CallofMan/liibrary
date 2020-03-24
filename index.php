0397a608aed9d33fc47b3f0f22c90de93c8da7c8
ivan <ivan.verbitsk@ya.ru>
2020-03-24 18:47:52 +0300

    uploaded the project on github

---
+++ index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style_index.css">
+    <title>Моя библиотека</title>
+</head>
+<body>
+    <div id="container">
+
+        <header id="header">
+
+            <p class="head_elem">Все книги</p>
+            <p class="head_elem">Буду читать</p>
+            <p class="head_elem">Прочитано</p>
+
+        </header>
+
+        <section id="functional_block">
+
+            <section id="search_block">
+
+                <input type="search" name="search" id="search" placeholder="Поиск">
+                <input type="button" value="Найти" id="button_search">
+
+            </section>
+            
+            <input type="button" value="Добавить книгу" id="add_book">
+
+        </section>
+
+        <main>
+
+            <article class="book">
+                
+                <div class="img">
+
+                    <img src="images/1451835669_andrea-guardino_lotr_hobbit_nazgul-king_2.jpg" alt="Не грузит" class="title_book">
+
+                </div>
+                
+                <h2>Пидор и трансвестит</h2>
+
+                <header class="manipulation_book">
+
+                    <img src="images/галочка.png" alt="Не грузит" id="read">
+                    <input type="button" value="Конспект" id="comment">
+
+                </header>
+
+            </article>
+
+            <article class="book">
+                
+                <div class="img">
+
+                    <img src="images/Мое фото 1.png" alt="Не грузит" class="title_book">
+
+                </div>
+                
+                <h2>Пидор и трансвестит</h2>
+
+                <header class="manipulation_book">
+
+                    <img src="images/галочка.png" alt="Не грузит" id="read">
+                    <input type="button" value="Конспект" id="comment">
+                    
+                </header>
+
+            </article>
+
+            <article class="book">
+                
+                <div class="img">
+
+                    <img src="images/я.jpg" alt="Не грузит" class="title_book">
+
+                </div>
+                
+                <h2>Пидор и трансвестит</h2>
+
+                <header class="manipulation_book">
+
+                    <img src="images/галочка.png" alt="Не грузит" id="read">
+                    <input type="button" value="Конспект" id="comment">
+                    
+                </header>
+
+            </article>
+
+            <article class="book">
+                
+                <div class="img">
+
+                    <img src="images/IMG_1159.jpg" alt="Не грузит" class="title_book">
+
+                </div>
+                
+                <h2>Пидор и трансвестит</h2>
+
+                <header class="manipulation_book">
+
+                    <img src="images/галочка.png" alt="Не грузит" id="read">
+                    <input type="button" value="Конспект" id="comment">
+                    
+                </header>
+
+            </article>
+
+            <article class="book">
+                
+                <div class="img">
+
+                    <img src="images/^1EEC08AC39E3E041A49FC60B52D152FEF6AFC3F800F102CBD0^pimgpsh_fullsize_distr.jpg" alt="Не грузит" class="title_book">
+
+                </div>
+                
+                <h2>Пидор и трансвестит</h2>
+
+                <header class="manipulation_book">
+
+                    <img src="images/галочка.png" alt="Не грузит" id="read">
+                    <input type="button" value="Конспект" id="comment">
+                    
+                </header>
+
+            </article>
+
+        </main>
+    </div>
+</body>
+</html>
