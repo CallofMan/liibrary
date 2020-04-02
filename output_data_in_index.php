d0f61aebdf948dfe411309a5f6af2484b937db83
CallofMan <ivan.verbitsk@ya.ru>
2020-04-02 18:48:09 +0300

    Добавил модалочку на вывод аннотаций при наведении на обложку

--- output_data_in_index.php
+++ output_data_in_index.php
@@ -6,9 +6,9 @@
 
     switch ($choiceCategory)
     {
-        case 1: $query = mysqli_query($connect, "SELECT id, name, author, date_of_writing FROM books WHERE status = 1 ORDER BY id DESC");    break;
-        case 2: $query = mysqli_query($connect, "SELECT id, name, author, date_of_writing FROM books WHERE status = 0 ORDER BY id DESC");    break;
-        case 3: $query = mysqli_query($connect, "SELECT id, name, author, date_of_writing FROM books ORDER BY id DESC");                     break;
+        case 1: $query = mysqli_query($connect, "SELECT id, name, author, annotation, date_of_writing FROM books WHERE status = 1 ORDER BY id DESC");    break;
+        case 2: $query = mysqli_query($connect, "SELECT id, name, author, annotation, date_of_writing FROM books WHERE status = 0 ORDER BY id DESC");    break;
+        case 3: $query = mysqli_query($connect, "SELECT id, name, author, annotation, date_of_writing FROM books ORDER BY id DESC");                     break;
     }
 
     while ($row = mysqli_fetch_assoc($query))
@@ -23,17 +23,18 @@
             
                 <div class="img">
 
-            <img src="data:' . $queryImage['type_image'] . ';base64,' . base64_encode($queryImage['image']) . '" alt="Нет обложки" class="title_book">
+                    <p>' . $row['annotation'] . '</p>
 
-            </div>
+                    <img src="data:' . $queryImage['type_image'] . ';base64,' . base64_encode($queryImage['image']) . '" alt="Нет обложки" class="title_book">
+
+                </div>
             
             <h2>"' . $row['name'] . '"</h2>
             <h3>' . $row['author'] . '</h3>
             <h4>' . $row['date_of_writing'] . '</h4>    
             <header class="manipulation_book">
 
-                <img src="images/галочка.png" alt="Не грузит" class="read">
-                <input type="button" value="Конспект" class="comment">
+                <input type="button" value="Детальное описание" class="comment">
 
             </header>
 
